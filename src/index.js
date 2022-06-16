03030841eac9288a3fdedfa143f59ba99b1910b8
Aron Galvan <arongalvan03@gmail.com>
2022-06-15 22:29:25 -0700

    commit tree 5574108b79532cae8854fe45037a0e34aa584cb5

--- src/index.js
+++ src/index.js
@@ -12,4 +12,115 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+const toyCollection = document.getElementById('toy-collection');
+
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(data => {
+
+    console.log(data);
+
+    for(const toy of data)
+    {
+      const card = document.createElement("div");
+      const cardH2 = document.createElement("h2");
+      const cardImg = document.createElement("img");
+      const cardP = document.createElement("p");
+      const cardButton = document.createElement("button");
+
+      card.classList.add("card");
+      cardH2.innerText = toy.name;
+      cardImg.src = toy.image;
+      cardImg.classList.add("toy-avatar")
+      cardP.innerText = toy.likes + " Likes";
+      cardButton.id = toy.id;
+      cardButton.classList.add("like-btn");
+      cardButton.innerText = "Like ❤️"
+
+      card.append(cardH2);
+      card.append(cardImg);
+      card.append(cardP);
+      card.append(cardButton);
+
+      toyCollection.append(card);
+    }
+
+    updateToy();
+  })
+  .catch(error => console.log(error));
+
+
+
+  toyFormContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const toy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    const configuration = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toy)
+    }
+
+    fetch("http://localhost:3000/toys", configuration)
+    .then(res => res.json())
+    .then(toy => {
+      const card = document.createElement("div");
+      const cardH2 = document.createElement("h2");
+      const cardImg = document.createElement("img");
+      const cardP = document.createElement("p");
+      const cardButton = document.createElement("button");
+
+      card.classList.add("card");
+      cardH2.innerText = toy.name;
+      cardImg.src = toy.image;
+      cardImg.classList.add("toy-avatar")
+      cardP.innerText = toy.likes + " Likes";
+      cardButton.id = toy.id;
+      cardButton.classList.add("like-btn");
+      cardButton.innerText = "Like ❤️"
+
+      card.append(cardH2);
+      card.append(cardImg);
+      card.append(cardP);
+      card.append(cardButton);
+
+      toyCollection.append(card);
+    })
+    .catch(error => console.log(error));
+  })
+
+
+  const updateToy = () => {
+    document.querySelectorAll('.like-btn').forEach(button => button.addEventListener('click', (e) => {
+  
+      const likesP = e.target.previousSibling;
+      
+      const toy = {
+        likes: parseFloat(likesP.innerText[0]) + 1,
+      }
+      const configuration = {
+        method: 'PATCH',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(toy)
+      }
+    
+      fetch(`http://localhost:3000/toys/${e.target.id}`, configuration)
+      .then(res => res.json())
+      .then(toy => {
+        likesP.innerText = `${toy.likes} Likes`;
+      })
+      .catch(error => console.log(error));
+    }))
+  }
 });
